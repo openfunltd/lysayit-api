108f85b8dae15510b12b850cbf0837c27f867b21
Ronny Wang <ronnywang@gmail.com>
2022-01-06 21:12:13 +0800

    add /speaker/{someone}/meet API

--- api.php
+++ api.php
@@ -9,7 +9,7 @@ $method = strtolower($matches[1]);
 $matches[2] = ltrim($matches[2], '/');
 $params = [];
 if ($matches[2]) {
-    $params = explode('/', $matches[2]);
+    $params = array_map('urldecode', explode('/', $matches[2]));
 }
 
 if ($method == 'meet') {
@@ -41,6 +41,39 @@ if ($method == 'meet') {
     }
     echo json_encode($records, JSON_UNESCAPED_UNICODE);
     exit;
+} elseif ($method == 'speaker' and $speaker = $params[0] and 'meet' == $params[1]) {
+    $cmd = [
+        'query' => array(
+            'term' => array('speaker' => $speaker),
+        ),
+        'size' => 10000,
+    ];
+    $obj = API::query('/speech/_search', 'GET', json_encode($cmd));
+    $meets = [];
+    $records = new StdClass;
+    $records->speeches = [];
+    foreach ($obj->hits->hits as $hit) {
+        $record = $hit->_source;
+        $meets[$record->meet_id] = true;
+        $records->speeches[] = $record;
+    }
+
+    $cmd = [
+        'query' => array(
+            'ids' => array('values' => array_keys($meets)),
+        ),
+        'size' => 10000,
+    ];
+    $obj = API::query('/meet/_search', 'GET', json_encode($cmd));
+    foreach ($obj->hits->hits as $hit) {
+        $record = $hit->_source;
+        $record->id = $hit->_id;
+        $record->extra = json_decode($record->extra);
+        $records->meets[] = $record;
+    }
+    echo json_encode($records, JSON_UNESCAPED_UNICODE);
+    exit;
+
 } elseif ($method == 'speaker' and $speaker  = $params[0]) {
     $cmd = [
         'query' => array(
