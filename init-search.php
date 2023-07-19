6e81061c3693447d9dcc767451ddb8b9bb95e8ab
Ronny Wang <ronnywang@gmail.com>
2023-07-19 17:15:09 +0800

    改用新的 elastic 寫法

--- init-search.php
+++ init-search.php
@@ -3,62 +3,25 @@
 include(__DIR__ . '/LYLib.php');
 include(__DIR__ . '/config.php');
 
-LYLib::dbQuery('', 'DELETE');
-LYLib::dbQuery('', 'PUT', '{"settings": { "analysis":{ "analyzer":{ "default":{"type":"cjk"} } }} }');
-LYLib::dbQuery('/meet/_mapping', 'DELETE', '{}');
-LYLib::dbQuery("/meet/_mapping", 'PUT', '{
- "meet" : {
-  "date_detection": false,
-  "properties" : {
-    "title": {"type" : "string", "index":"analyzed", "analyzer":"cjk"},
-    "term": {"type": "integer"},
-    "sessionPeriod": {"type": "integer"},
-    "date": {"type": "integer"},
-    "extra" : {"type" : "string"}
-  }
- }
-}');
-LYLib::dbQuery('/person/_mapping', 'DELETE', '');
-LYLib::dbQuery("/person/_mapping", 'PUT', '{
- "person" : {
-  "date_detection": false,
-  "properties" : {
-    "term": {"type": "integer"},
-    "name": {"type" : "string", "index":"analyzed", "analyzer":"cjk"},
-    "type": {"type" : "integer"},
-    "extra" : {"type" : "string"}
-  }
- }
-}');
+try {
+    LYLib::dropIndex('meet');
+    LYLib::dropIndex('person');
+    LYLib::dropIndex('speech');
+    LYLib::dropIndex('vote');
+} catch (Exception $e) {
+}
+LYLib::createIndex('meet', [
+    "date_detection" => false,
+]);
 
-LYLib::dbQuery("/speech/_mapping", 'DELETE', '');
-LYLib::dbQuery("/speech/_mapping", 'PUT', '{
- "speech" : {
-  "date_detection": false,
-  "properties" : {
-    "term": {"type": "integer"},
-    "meet_id": {"type": "string"},
-    "lineno": {"type" : "integer"},
-    "speaker": {"type": "string", "index": "not_analyzed"},
-    "content": {"type" : "string", "index":"analyzed", "analyzer":"cjk"},
-    "extra" : {"type" : "string"}
-  }
- }
-}');
+LYLib::createIndex('person', [
+    "date_detection" => false,
+]);
 
-LYLib::dbQuery('/vote/_mapping', 'DELETE', '');
-LYLib::dbQuery('/vote/_mapping', 'PUT', json_encode([
-    'vote' => [
-        'date_detection' => false,
-        'properties' => [
-            'term' => ['type' => 'integer'],
-            'meet_id' => ["type" => "string"],
-            'line_no' => ['type' => 'integer'],
-            '贊成' => ['type' => 'string', 'index' => 'not_analyzed'],
-            '反對' => ['type' => 'string', 'index' => 'not_analyzed'],
-            '棄權' => ['type' => 'string', 'index' => 'not_analyzed'],
-            'extra' => ['type' => 'string'],
-        ],
-    ],
-]));
-echo json_encode($ret, JSON_UNESCAPED_UNICODE) . "\n";
+LYLib::createIndex('speech', [
+    "date_detection" => false,
+]);
+
+LYLib::createIndex('vote', [
+    "date_detection" => false,
+]);
